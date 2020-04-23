7ff456fe7d1bfdf11f6f390c166d519b3a25955c
leylifrida230 <55417475+leylifrida230@users.noreply.github.com>
2020-04-23 14:38:27 +0700

    Create web covid about.php

---
+++ web covid about.php
@@ -0,0 +1,61 @@
+<?php
+    include 'aset/header.php';
+    include 'koneksi.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About Me</title>
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+	    <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->	
+	    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->	
+	    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->	
+	    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="css/util.css">
+	    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+
+    </head>
+    <body>
+        <div class="limiter">
+        <div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+        
+            <div class="form">
+                
+                <p class="tulisan">Its About Me</p><br>
+
+                <form action="cek_login.php" method="post">
+
+                    <label2>Nama : Leyli Frida Sofi Eka Joti</label2><br><br>
+
+                    <label2>Kelas : XRPL1</label2><br><br>
+
+                    <label2>No : 25</label2><br><br>
+
+                    <label2>TELKOM SCHOOL MALANG</label2>
+                    
+                </form>
+            </div>
+    </body>
+
+</html>
