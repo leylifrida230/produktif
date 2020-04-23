b68c841bad58629828fb9ca070f11e7e2e86c143
leylifrida230 <55417475+leylifrida230@users.noreply.github.com>
2020-04-23 14:44:13 +0700

    Create web covid contact.php

---
+++ web covid contact.php
@@ -0,0 +1,45 @@
+<?php
+    include 'aset/header.php';
+    include 'koneksi.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contact Me</title>
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+	    <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->	
+	    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->	
+	    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->	
+	    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="css/util.css">
+	    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+
+    </head>
+    <body>
+        <div class="limiter">
+        <div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+        <h3 align="center">"Contact itu kunci motor bukan sih?"</h3>
+    </body>
+
+</html>
