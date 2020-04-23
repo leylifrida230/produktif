b6835d66a689c87ec738cb1e93c085a72816a8e3
leylifrida230 <55417475+leylifrida230@users.noreply.github.com>
2020-04-23 14:32:58 +0700

    Create web covid header.php

---
+++ web covid header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="http://localhost/covid/aset/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/covid/aset/fontawesome/css/all.min.css">
+
+    <title>IndoCOVID</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"><i class="fas fa-book-reader text-white mr-2"></i>IndoCovid </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+        <a class="nav-item nav-link" href="http://localhost/covid/login.php">Dashboard</a>
+        <a class="nav-item nav-link" href="http://localhost/covid/about.php">About</a>
+        <a class="nav-item nav-link" href="http://localhost/covid/contact.php" >Contact</a>
+        <a class="nav-item nav-link" href="http://localhost/covid/logout.php" >Logout</a>
+        </div>
+    </div>
+    </nav>
+
+    
