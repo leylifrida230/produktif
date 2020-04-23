149002ce5a0c35d87016059d41e4ad5b93f5b5ed
leylifrida230 <55417475+leylifrida230@users.noreply.github.com>
2020-04-23 14:40:32 +0700

    Create web covid akhir.php

---
+++ web covid akhir.php
@@ -0,0 +1,52 @@
+<?php
+    include 'aset/header.php';
+    include 'koneksi.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Halaman Akhir</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+	    <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->	
+	    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->	
+	    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->	
+	    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+	    <link rel="stylesheet" type="text/css" href="css/util.css">
+	    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+    </head>
+    <body>
+    <div class="limiter">
+        <div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+            <h2 align="center">Terimakasih Telah Menjawab Pertanyaan di IndoCOVID</h2><br>
+                <h2 align="center">Terimakasih telah menggunakan test ini, semoga 
+                    anda sehat selalu. tetap waspada, tidak perlu panik, dirumah saja, jangan jalan jalan, 
+                    nanti kena corona, siapa yang susah, banyak yang susah kan, jangan ngeyel jalan jalan, jangan lupa pake masker kalo keluar rumah 
+                    oke oke oke salam santuy
+                </h2>
+
+                <br/>Klik untuk <a href='logout.php'>Logout</a>
+        </div>
+    </div>
+    </body>
+</html>
+
